3ced34c06bc9375857935feb8ad9e7947a37fb99
victor_nxx <victor_nxx@protonmail.com>
2025-08-17 23:40:45 +0100

    feat: created core and base apps

--- localhub/settings.py
+++ localhub/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'users',
+    'news',
+    'events',
 ]
 
 MIDDLEWARE = [
