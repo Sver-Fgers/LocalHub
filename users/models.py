e135310766ac143492bc2e08cf45d75f8dc8dcf3
root <root@localhost.localdomain>
2025-08-30 13:36:31 +0000

    add user profile features

--- users/models.py
+++ users/models.py
@@ -21,3 +21,12 @@ class User(AbstractUser, TimeStampedModel):
         if self.is_member:
             roles.append("Member")
         return f"{self.username} ({', '.join(roles)})"
+
+class UserProfile(models.Model):
+    # ONE USER HAS ONLY ONE PROFILE
+	user = models.OneToOneField(User, primary_key=True, verbose_name='user', related_name='profile', on_delete=models.CASCADE)
+	name = models.CharField(max_length=30, blank=True, null=True)
+	bio = models.TextField(max_length=500, blank=True, null=True)
+	birth_date=models.DateField(null=True, blank=True)
+	location = models.CharField(max_length=100, blank=True, null=True)
+	picture = models.ImageField(upload_to='uploads/profile_pictures', default='uploads/profile_pictures/default.png', blank=True)
